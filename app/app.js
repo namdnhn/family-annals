f21b170438f6e1c38c2a01fd843c72d08160d1ff
tolamson2811 <100157005+tolamson2811@users.noreply.github.com>
2023-11-27 19:52:46 +0700

    test CI

--- app/app.js
+++ app/app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
         "Access-Control-Allow-Headers",
         "Content-Type, Authorization"
     );
-    next();
+    next(); 
 });
 
 app.use("/auth", authRoutes);
